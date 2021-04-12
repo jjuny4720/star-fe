5c9a194bc3413840f678c50be48e125ea0ad7825
jjuny4720 <43638423+jjuny4720@users.noreply.github.com>
2021-04-12 19:17:25 +0900

    Update quiz4.js

--- 06/quiz4.js
+++ 06/quiz4.js
@@ -3,19 +3,22 @@ const $box = document.getElementById('box');
 let offset = {x:0, y:0};
 let isClick = false;
 
-$box.addEventListener('mousedown', (event)=> {
+$box.addEventListener('mousedown', (event)=> 
+{
     isClick = true;
     console.log($box.offsetLeft);
     offset.x = $box.offsetLeft - event.clientX;
     offset.y = $box.offsetTop - event.clientY;
 });
 
-$box.addEventListener('mouseup', (event) => {
+$box.addEventListener('mouseup', (event) => 
+{
     isClick = false;
 
 });
 
-document.body.addEventListener('mousemove', (event) => {
+document.body.addEventListener('mousemove', (event) => 
+{
     console.log('mousemove');
     if(!isClick) return;
     console.log(event.clientX, event.clientY);
