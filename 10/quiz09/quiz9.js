59705dea2cdb98a318b64465f6d76015d382de57
jjuny4720 <43638423+jjuny4720@users.noreply.github.com>
2021-05-19 17:54:12 +0900

    Create quiz9.js

---
+++ 10/quiz09/quiz9.js
@@ -0,0 +1,35 @@
+const $url = document.querySelector('#url');
+const $btn = document.querySelector('#btn');
+const $log = document.querySelector('#log');
+
+const changeLog = (text) => {
+  $log.innerHTML = text;
+};
+
+const fetchUrl = () => {
+  const url = $url.value;
+
+  if (!url.trim()) {
+    alert('url을 입력하시오');
+    $url.value = '';
+  } 
+    else 
+  {
+    fetch(url)
+      .then((res) => {
+        res.text().then((text) => changeLog(text));
+      })
+      .catch((err) => {
+        $log.value = err;
+      });
+  }
+};
+
+const enterPress = (e) => {
+  if (e.keyCode === 13) {
+    fetchUrl();
+  }
+};
+
+$url.addEventListener('keypress', enterPress);
+$btn.addEventListener('click', fetchUrl);
