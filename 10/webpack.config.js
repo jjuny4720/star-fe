cc914e367e001ef381b417d3ecffbf76447cfc9a
jjuny4720 <43638423+jjuny4720@users.noreply.github.com>
2021-05-26 22:29:08 +0900

    Create webpack.config.js

---
+++ 10/webpack.config.js
@@ -0,0 +1,17 @@
+  
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'app.js',
+    path: `${__dirname}/dist`,
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [new HtmlWebpackPlugin({
+      template: './src/index.html',
+  })],
+};
